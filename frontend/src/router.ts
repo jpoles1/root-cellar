402a359fe89815177920c4d54e40816d2fb50756
Jordan Poles <jpdev.noreply@gmail.com>
2020-06-09 11:43:48 -0400

    Drop old files

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -39,12 +39,5 @@ export default new Router({
 			component: RecipeEditor,
 			props: true,
 		},
-		{
-			path: "/about",
-			// route level code-splitting
-			// this generates a separate chunk (about.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			component: () => import(/* webpackChunkName: "about" */ "./views/About.vue"),
-		},
 	],
 })
