488ed5c6035fefd981662bba7b1fbf5396fc7139
Jordan Poles <jpdev.noreply@gmail.com>
2020-06-17 08:55:32 -0400

    Release 0.1

---
+++ frontend/dist/precache-manifest.a5d9319a97ab641039e406076d5bcfbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cf3ed0236a5057ed4f81",
+    "url": "/js/chunk-vendors.1dd1e8bb.js"
+  },
+  {
+    "revision": "9ef2d5109c92d3f3cd05",
+    "url": "/js/app.b2a0e971.js"
+  },
+  {
+    "revision": "d859f87b0d62e2e2d8ef0b872b0d8039",
+    "url": "/index.html"
+  },
+  {
+    "revision": "696c081ae56a5d4674d5da24e7810ab7",
+    "url": "/img/google_login.png"
+  },
+  {
+    "revision": "0d9c78166fd8c428235defa4e70291a9",
+    "url": "/img/404.png"
+  },
+  {
+    "revision": "cf3ed0236a5057ed4f81",
+    "url": "/css/chunk-vendors.174aa8cf.css"
+  },
+  {
+    "revision": "9ef2d5109c92d3f3cd05",
+    "url": "/css/app.38b5d437.css"
+  }
+];
