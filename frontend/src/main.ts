95c2caa7162a45ffa418d95a3ef14e0dc7ae51eb
Jordan Poles <jpdev.noreply@gmail.com>
2020-08-26 21:09:02 -0400

    Fixing toast

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,12 +5,12 @@ import router from "./router";
 import store from "./store";
 
 import vuetify from "./plugins/vuetify";
-import VuetifyToast from "vuetify-toast-snackbar";
+import VuetifyToast from "vuetify-toast-snackbar-ng";
 import headful from "vue-headful";
 
 Vue.use(VuetifyToast, {
 	x: "left",
-	timeout: 2500,
+	timeout: 2000,
 	color: "#004D40", // teal darken-4
 });
 Vue.component("headful", headful);
